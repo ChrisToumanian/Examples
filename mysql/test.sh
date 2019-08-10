ae67c04a7050b284e996ff9a4adf592aca04d0f2
Christopher Toumanian <cctoumanian@gmail.com>
2019-08-09 19:48:51 -0700

    Added MySQL bash info.

---
+++ mysql/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#user=Chris
+#password=Waunuma64!
+database=minecraft
+#mysql -u "$user" -p"$password" -D "$database" -e "SELECT email FROM users WHERE username='Pandazoic';" -sN >> out.txt
+
+output=$(mysql --defaults-extra-file=config.cnf -D "$database" -e "SELECT username FROM users;" -sN)
+
+echo "$output";
